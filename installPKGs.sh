b515a226e9d8e08021322f660e2f56926d62afe7
Mason Bergstrom <mason.o.bergstrom@gmail.com>
2017-08-09 19:24:26 -0700

    Updated installPKGs.sh auto configure two more files, '/ect/lirc/hardware.conf' and '/etc/modules.' Also improved upon the safeness of running the script. It used to double add lines if the default line wasn't there anymore when you ran the script more than once.

--- installPKGs.sh
+++ installPKGs.sh
@@ -1,8 +1,22 @@
 #Install any missing packages needed for IoTRemote
 sudo apt-get install git rsync lirc nodejs npm
 
-#Apply config changes to active lirc talking to the IR Shield
-sudo sed -i 's/#dtoverlay=lirc-rpi/dtoverlay=lirc-rpi:gpio_in_pin=18,gpio_out_pin=17/' /boot/config.txt
+#Apply config changes to activate lirc talking to the IR Shield
+sudo sed -i 's/#dtoverlay=.*/dtoverlay=lirc-rpi:gpio_in_pin=18,gpio_out_pin=17/' /boot/config.txt
+#Setting up the hardware.conf file to also activate lirc talking to the IR Shield
+sudo sed -i 's/LIRCD_ARGS=.*/LIRCD_ARGS="--device \/dev\/lirc0 -uinput -listen"/' /etc/lirc/hardware.conf
+sudo sed -i 's/DRIVER=.*/DRIVER="default"/' /etc/lirc/hardware.conf
+sudo sed -i 's/DEVICE=.*/DEVICE="\/dev\/lirc0"/' /etc/lirc/hardware.conf
+sudo sed -i 's/MODULES=.*/MODULES="lirc_rpi"/' /etc/lirc/hardware.conf
+#Final file for configuring lirc to talk to the IR Shield, the modules files
+if ! grep -q "lirc_rpi gpio_in_pin=.* gpio_out_pin=.*" /etc/modules ; then
+    sudo sed -i '0,/^$/{s/^$/\nlirc_rpi gpio_in_pin=18 gpio_out_pin=17\n/}' /etc/modules
+fi
+if ! grep -q "lirc_dev" /etc/modules ; then
+    sudo sed -i '0,/^$/{s/^$/\nlirc_dev\n/}' /etc/modules
+fi
 
 #Apply changes to rc.local so lirc start properly at boot
-sudo sed -i '0,/^$/{s/^$/\n# Start LIRC properly\nsudo lircd --device \/dev\/lirc0\n/}' /etc/rc.local
+if ! grep -q "sudo lircd --device /dev/lirc.*" /etc/rc.local ; then
+    sudo sed -i '0,/^$/{s/^$/\n# Start LIRC properly\nsudo lircd --device \/dev\/lirc0\n/}' /etc/rc.local
+fi
